b53639afb7935f9bafc009862427f0c2f2eb1942
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-28 23:33:07 +0300

    import components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://copyrightservice.co.uk/_f/4815/9197/8330/logo-1933884_640.jpg" alt="logo"/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://cdn.wallpapersafari.com/61/41/c6lhFM.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
