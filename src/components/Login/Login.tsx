ba2e912066264b4174199d354d6a2d377e50fac0
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-26 23:18:57 +0300

    feat: added Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,6 +4,22 @@ export const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+          <form action="">
+            <div>
+              <input type="text" placeholder={'Login'}/>
+            </div>
+            <div>
+              <input type="password" placeholder={'Password'}/>
+            </div>
+            <div>
+              <input type="checkbox"/> remember me
+            </div>
+            <div>
+              <button>
+                login
+              </button>
+            </div>
+          </form>
         </div>
     );
 };
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,13 +5,15 @@ import sidebarReducer from "./reducers/sidebar-reducer";
 import {usersReducer} from "./reducers/users-reducer";
 import {authReducer} from "./reducers/auth-reducer";
 import thunkMiddleWare from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 let rootReducer = combineReducers({
-    profilePage: profileReducer,
-    dialogsPage: dialogsReducer,
-    sidebar: sidebarReducer,
-    usersPage: usersReducer,
-    auth:authReducer
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  sidebar: sidebarReducer,
+  usersPage: usersReducer,
+  auth: authReducer,
+  form: formReducer
 })
 
 
