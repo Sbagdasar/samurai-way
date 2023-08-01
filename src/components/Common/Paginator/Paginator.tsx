73b01b34af4ea4dab3bd4033f4855a91e0c644b8
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-08-01 21:20:22 +0300

    refactor: update Paginator.tsx

--- src/components/Common/Paginator/Paginator.tsx
+++ src/components/Common/Paginator/Paginator.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Paginator.module.css";
 type PaginatorPT ={
   totalItemsCount:number
   pageSize:number
   onSetCurrentPage:(p:number)=>void
   currentPage:number
+  portionSize?: number
 }
-export const Paginator = ({totalItemsCount, pageSize, onSetCurrentPage, currentPage}:PaginatorPT) => {
+export const Paginator = ({totalItemsCount, pageSize, onSetCurrentPage, currentPage, portionSize = 10}:PaginatorPT) => {
+
+
+
   let pagesCount = Math.ceil(totalItemsCount / pageSize);
   let pages = Array.from({length: +pagesCount}, (_, i: number) => +i + 1)
+  let portionCount = Math.ceil(pagesCount/portionSize)
+  let [portionNumber, setPortionNumber] = useState(1)
+  let leftPortionPageNumber = (portionNumber-1) * portionSize + 1
+  let rightPortionPageNumber = portionNumber * portionSize
 
   return (
     <div className={s.pagination}>
       {
-        pages.map(p => {
+        pages.filter(p => p>= leftPortionPageNumber && p<=rightPortionPageNumber)
+          .map(p => {
           return (
             <span key={p}
                   className={currentPage === p ? s.selectedPage : s.page}
@@ -21,6 +30,9 @@ export const Paginator = ({totalItemsCount, pageSize, onSetCurrentPage, currentP
           )
         })
       }
+      {leftPortionPageNumber>1 && <button onClick={()=>{setPortionNumber(portionNumber-1)}}>PREV</button>}
+      {portionCount>portionNumber && <button onClick={()=>{setPortionNumber(portionNumber+1)}}>NEXT</button>}
+
     </div>
   );
 };
